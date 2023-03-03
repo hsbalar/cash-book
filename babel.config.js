4dfb0b3aba107f07d768864b405f0621ce47b033
Hitesh <hsbalar@gmail.com>
2023-03-03 11:41:04 +0530

    Add ios & android related config

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,16 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      [
-        "dotenv-import",
-        {
-          moduleName: "@env",
-          path: ".env",
-          blocklist: null,
-          allowlist: null,
-          safe: false,
-          allowUndefined: false,
-        },
-      ],
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'dotenv-import',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        safe: false,
+        allowUndefined: false,
+      },
     ],
-  };
+  ],
 };
